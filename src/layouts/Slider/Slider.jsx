faa8dc2879658d5ccbc53b74650c74daa6ef2629
matysikgeorge <matysikgeorge1995@gmail.com>
2024-09-27 16:42:45 +0300

    слияние финал

--- src/layouts/Slider/Slider.jsx
+++ src/layouts/Slider/Slider.jsx
@@ -2,6 +2,9 @@ import s from "./Slider.module.scss";
 import slider1 from "../../assets/imgs/slider/slider1.webp";
 import slider2 from "../../assets/imgs/slider/slider2.webp";
 import slider3 from "../../assets/imgs/slider/slider3.webp";
+import mobSlider1 from "../../assets/imgs/slider/mobile__slider.webp";
+import mobSlider2 from "../../assets/imgs/slider/mobile__slider2.webp";
+
 import { Container } from "../Container/Container";
 import { useContext } from "react";
 import { WindowWidthContext } from "../../providers/WindowWidthContext/WindowWidthContext";
@@ -21,13 +24,25 @@ export const Slider = () => {
       >
         <swiper-slide lazy="true">
           {" "}
-          <img src={slider1} className={s.slider} loading="lazy" />
+          {width > 500 ? (
+            <img src={slider1} className={s.slider} loading="lazy" />
+          ) : (
+            <img src={mobSlider1} className={s.slider} loading="lazy" />
+          )}
         </swiper-slide>
         <swiper-slide lazy="true">
-          <img src={slider2} className={s.slider} loading="lazy" />
+          {width > 500 ? (
+            <img src={slider2} className={s.slider} loading="lazy" />
+          ) : (
+            <img src={mobSlider2} className={s.slider} loading="lazy" />
+          )}
         </swiper-slide>
         <swiper-slide lazy="true">
-          <img src={slider3} className={s.slider} loading="lazy" />
+          {width > 500 ? (
+            <img src={slider3} className={s.slider} loading="lazy" />
+          ) : (
+            <img src={mobSlider1} className={s.slider} loading="lazy" />
+          )}
         </swiper-slide>
       </swiper-container>
     </>
